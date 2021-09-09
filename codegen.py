7b78be79f1a00eb3d5ef5eacd1cbb821d6c0c035
Adam Blanchet <ab59@sanger.ac.uk>
2021-09-09 12:02:56 +0100

    Add codegen and generated code.

---
+++ codegen.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+
+PW = os.environ["MYSQL_PW"]
+USER = os.environ["MYSQL_USER"]
+HOST = os.environ["MYSQL_HOST"]
+PORT = os.environ["MYSQL_PORT"]
+DBNAME = os.environ["MYSQL_DBNAME"]
+
+url = "mysql+pymysql://{user}:{passw}@{host}:{port}/{db}?charset=utf8mb4".format(
+    user=USER, passw=PW, host=HOST, port=PORT, db=DBNAME
+)
+
+# Generate the declarative mappings.
+subprocess.run(
+    [
+        "sqlacodegen",
+        url,
+        "--outfile",
+        "generated.py",
+    ]
+)
+
+# Workaround issue with CHAR.
+subprocess.run(
+    ["sed", "-i.bu", "/sqlalchemy.dialects.mysql/s/CHAR, *//", "generated.py"]
+)
+os.remove("generated.py.bu")
+
+# Move the generated file.
+os.rename("generated.py", "ml_warehouse/ml_warehouse_schema_new.py")
